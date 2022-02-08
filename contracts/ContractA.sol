417c5bc36c1497d3540357d928f37b520d9c5bf0
0xeth0x <sheraz2392@gmail.com>
2022-02-08 15:16:46 +0500

    format code

--- contracts/ContractA.sol
+++ contracts/ContractA.sol
@@ -1,10 +1,10 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.9;
 
-import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-import { ContractB } from "./ContractB.sol";
+import {ContractB} from "./ContractB.sol";
 
 contract ContractA is Ownable {
     using SafeERC20 for IERC20;
@@ -13,7 +13,7 @@ contract ContractA is Ownable {
 
     event ContractBSet(address newContractB);
 
-    function setContractB(address _contractB) 
+    function setContractB(address _contractB)
         external
         onlyOwner
     {
--- contracts/ContractB.sol
+++ contracts/ContractB.sol
@@ -1,18 +1,18 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.9;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ContractB is Ownable {
-	address public contractA;
+    address public contractA;
 
-	// token => user => amount
-	mapping(address => mapping(address => uint256)) public tokenToUserDeposit;
+    // token => user => amount
+    mapping(address => mapping(address => uint256)) public tokenToUserDeposit;
 
-	event ContractASet(address newContractA);
-	event DepositRecorded(address token, address user, uint256 amount);
+    event ContractASet(address newContractA);
+    event DepositRecorded(address token, address user, uint256 amount);
 
-	function setContractA(address _contractA) 
+    function setContractA(address _contractA)
         external
         onlyOwner
     {
@@ -25,36 +25,36 @@ contract ContractB is Ownable {
         emit ContractASet(_contractA);
     }
 
-	function recordDeposit(
-		address _user,
-		address _token,
-		uint256 _amount
-	)
-		external
-		onlyOwnerOrContractA
-	{
-		require(
-			_user != address(0)
-			&& _token != address(0),
-			"ContractB::recordDeposit: invalid function parameters."
+    function recordDeposit(
+        address _user,
+        address _token,
+        uint256 _amount
+    )
+        external
+        onlyOwnerOrContractA
+    {
+        require(
+            _user != address(0)
+            && _token != address(0),
+            "ContractB::recordDeposit: invalid function parameters."
 
-		);
+        );
 
-		tokenToUserDeposit[_token][_user] += _amount;
+        tokenToUserDeposit[_token][_user] += _amount;
 
-		emit DepositRecorded(_token, _user, _amount);
-	}
+        emit DepositRecorded(_token, _user, _amount);
+    }
 
-	function _onlyOwnerOrContractA() private view {
-		require(
-			msg.sender == owner()
-			|| msg.sender == contractA,
-			"ContractB::onlyOwnerOrContractA: invalid caller."
-		);
-	}
+    function _onlyOwnerOrContractA() private view {
+        require(
+            msg.sender == owner()
+            || msg.sender == contractA,
+            "ContractB::onlyOwnerOrContractA: invalid caller."
+        );
+    }
 
-	modifier onlyOwnerOrContractA {
-		_onlyOwnerOrContractA();
-		_;
-	}
+    modifier onlyOwnerOrContractA {
+        _onlyOwnerOrContractA();
+        _;
+    }
 }
